77a5dd8ad6a8b0c95f49ee399d8a7559b14e3804
Gustavoo1122 <gustavope883@gmail.com>
2025-09-02 03:38:13 -0300

    feat: Adiciona detalhes dinâmicos dos Pokémons

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,23 +1,21 @@
 function loadDetailPokemon (pokemon){
-    console.log(pokemon);
-
     const newHtml = `
                       <div class="pokemonDetailHeader">
-                        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="Bulbasaur">
+                        <img src="${pokemon.image}" class="${pokemon.type}" alt="Bulbasaur">
                         <button id="closeDetailPokemon">✖</button>
                       </div>
 
-                      <div id="pokemonDetailName"><p >Bulbasaur</p></div>
+                      <div id="pokemonDetailName"><p>${pokemon.name}</p></div>
 
                       <div id="pokemonDetailLine"></div>
 
                       <div class="pokemonDetailBody">
                         <p class="title">Weight</p>
-                        <p class="description">6</p>
+                        <p class="description">${pokemon.weight}</p>
                         <p class="title">Height</p>
-                        <p class="description">7.92</p>
+                        <p class="description">${pokemon.height}</p>
                         <p class="title">Skills</p>
-                        <p class="description">Overgrow, Chlorophyl</p>
+                        <p class="description">${pokemon.skills.join(', ')}</p>
                       </div>
                     `;
     modal.innerHTML = newHtml;
@@ -25,12 +23,11 @@ function loadDetailPokemon (pokemon){
 
 let pokemons = [];
 
-
-
 const overlay = document.getElementById('modalOverlay');
+const closeDetailPokemon = document.getElementById('closeDetailPokemon');
 
 pokeApi.getPokemons().then((pokemonsDetails) => {
-  pokemons = pokemonsDetails; // todos os pokemons carregados ficam aqui
+  pokemons = pokemonsDetails; // Todos os pokemons carregados ficam aqui
 });
 
 pokemonList.addEventListener("click", (event) => {
@@ -44,18 +41,13 @@ pokemonList.addEventListener("click", (event) => {
 
   const pokemonDetail = pokemons.find(p => p.number == pokemonNumber);
 
-  console.log(pokemonNumber);
   loadDetailPokemon(pokemonDetail);
-  
-  
   
   overlay.addEventListener("click", (event) => {
     if(event.target === overlay)
       overlay.style.display = 'none';
   })
 })
-
-const closeDetailPokemon = document.getElementById('closeDetailPokemon');
 
 closeDetailPokemon.addEventListener("click", (event) => {
   const clickedCloseDetail = event.target.closest('.closeDetailPokemon');
